1b459be2e46c34c86a01ea9d2e0ead9b311bc4f4
Ilya Martynenko <36264807+userpator94@users.noreply.github.com>
2018-11-18 18:40:07 +0400

    Add files via upload
    есть и работает treeview 
    получает нормальный путь из treeview

---
+++ test task/test task/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace test_task
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.задатьКореньToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.анализToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.закрытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.фурьеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem,
+            this.справкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(782, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.очиститьToolStripMenuItem,
+            this.задатьКореньToolStripMenuItem,
+            this.анализToolStripMenuItem,
+            this.закрытьToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // очиститьToolStripMenuItem
+            // 
+            this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+            this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.очиститьToolStripMenuItem.Text = "Очистить";
+            // 
+            // задатьКореньToolStripMenuItem
+            // 
+            this.задатьКореньToolStripMenuItem.Name = "задатьКореньToolStripMenuItem";
+            this.задатьКореньToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.задатьКореньToolStripMenuItem.Text = "Изменить корень";
+            this.задатьКореньToolStripMenuItem.Click += new System.EventHandler(this.задатьКореньToolStripMenuItem_Click);
+            // 
+            // анализToolStripMenuItem
+            // 
+            this.анализToolStripMenuItem.Name = "анализToolStripMenuItem";
+            this.анализToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.анализToolStripMenuItem.Text = "Анализ";
+            // 
+            // закрытьToolStripMenuItem
+            // 
+            this.закрытьToolStripMenuItem.Name = "закрытьToolStripMenuItem";
+            this.закрытьToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.закрытьToolStripMenuItem.Text = "Закрыть";
+            this.закрытьToolStripMenuItem.Click += new System.EventHandler(this.закрытьToolStripMenuItem_Click);
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.фурьеToolStripMenuItem});
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // фурьеToolStripMenuItem
+            // 
+            this.фурьеToolStripMenuItem.Name = "фурьеToolStripMenuItem";
+            this.фурьеToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.фурьеToolStripMenuItem.Text = "Фурье";
+            this.фурьеToolStripMenuItem.Click += new System.EventHandler(this.фурьеToolStripMenuItem_Click);
+            // 
+            // treeView
+            // 
+            this.treeView.AllowDrop = true;
+            this.treeView.HotTracking = true;
+            this.treeView.Indent = 10;
+            this.treeView.Location = new System.Drawing.Point(12, 27);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(211, 339);
+            this.treeView.TabIndex = 1;
+            this.treeView.ItemDrag += new System.Windows.Forms.ItemDragEventHandler(this.treeView_ItemDrag);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(301, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(412, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox1_DragEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 378);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.treeView);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Test task";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem задатьКореньToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem анализToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem закрытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem фурьеToolStripMenuItem;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ test task/test task/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_task
+{
+    public partial class Form1 : Form
+    {
+        private static string rootpath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+        public Form1()
+        {
+            InitializeComponent();
+            ListDirectory(treeView, rootpath);
+        }
+
+        private void закрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GC.Collect();
+            Application.Exit();
+        }
+
+        private void задатьКореньToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogresult = this.folderBrowserDialog1.ShowDialog();
+            if (dialogresult == DialogResult.OK)
+            {
+                rootpath = folderBrowserDialog1.SelectedPath.ToString();
+                ListDirectory(treeView, rootpath);
+            }
+            
+        }
+
+        private void фурьеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://google.gik-team.com/?q=%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D0%B7+%D1%84%D1%83%D1%80%D1%8C%D0%B5");
+        }
+
+        private void ListDirectory(TreeView treeView, string path)
+        {
+            treeView.Nodes.Clear();
+            var rootDirectoryInfo = new DirectoryInfo(path);
+            treeView.Nodes.Add(CreateDirectoryNode(rootDirectoryInfo));
+        }
+        private static TreeNode CreateDirectoryNode(DirectoryInfo directoryInfo)
+        {
+            var extension = "";
+            var directoryNode = new TreeNode(directoryInfo.Name);
+            foreach (var directory in directoryInfo.GetDirectories())
+            {
+                if (imagiesExist(directory))
+                    directoryNode.Nodes.Add(CreateDirectoryNode(directory));
+            }
+                
+            foreach (var file in directoryInfo.GetFiles())
+            {   //.bmp, .jpg, .png, .gif, .tiff
+                extension = Path.GetExtension(file.ToString());
+                if (extension.Equals(".bmp") || extension.Equals(".jpg") || extension.Equals(".png") 
+                    || extension.Equals(".gif") || extension.Equals(".tiff"))
+                    directoryNode.Nodes.Add(new TreeNode(file.Name));
+            }                
+            return directoryNode;
+        }
+        private static bool imagiesExist(DirectoryInfo directoryInfo)
+        {
+            var extension = "";
+            foreach (var file in directoryInfo.GetFiles())
+            {   //.bmp, .jpg, .png, .gif, .tiff
+                extension = Path.GetExtension(file.ToString());
+                if (extension.Equals(".bmp") || extension.Equals(".jpg") || extension.Equals(".png")
+                    || extension.Equals(".gif") || extension.Equals(".tiff"))
+                    return true;
+            }
+            return false;
+        }
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+            string imagepath = treeView.SelectedNode.FullPath.ToString();
+            int sl = rootpath.LastIndexOf('\\');
+            string ss = rootpath.Substring(0, sl+1);
+            textBox1.Text = ss + imagepath;
+        }
+
+        private void treeView_ItemDrag(object sender, System.Windows.Forms.ItemDragEventArgs e)
+        {
+            DoDragDrop(e.Item, DragDropEffects.Move);
+        }
+
+        
+    }
+}
